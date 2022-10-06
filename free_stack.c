4359abfb4f684f3fd11f295a64f3c949ad213c67
kasahh <bunmiwhesu@yahoo.com>
2022-10-06 20:40:24 +0000

    monty ALX task

---
+++ free_stack.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+* free_stack - frees a doubly linked list
+*
+* @head: head of stack
+*
+* Return: nil
+*/
+void free_stack(stack_t *head)
+{
+	stack_t *aux;
+
+	aux = head;
+	while (head)
+	{
+		aux = head->next;
+		free(head);
+		head = aux;
+	}
+}
